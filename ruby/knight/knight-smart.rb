9172f39fcba8ccfbe6517f3d970f21770f9eaaf5
John Rose <johnedmundrose@gmail.com>
2025-11-20 13:07:41 -0500

    Edge conditions; unwinnable states

--- ruby/knight/knight-smart.rb
+++ ruby/knight/knight-smart.rb
@@ -5,17 +5,11 @@ require "benchmark/ips"
 puts "Hello, from Knight III... ✌️"
 
 Point = Data.define(:x, :y) do
-  def +(other)
-    Point.new(x + other.x, y + other.y)
-  end
+  def +(other) = Point.new(x + other.x, y + other.y)
 
-  def to_s
-    "(#{x},#{y})"
-  end
+  def to_s = "(#{x},#{y})"
 
-  def inspect
-    to_s
-  end
+  def inspect = to_s
 
   def inbounds?(size)
     x.between?(0, size - 1) && y.between?(0, size - 1)
@@ -33,16 +27,14 @@ DELTAS = [
   Point.new(x: -1, y: -2)
 ].freeze
 
-def render_board(board:, position:, new_position:)
+def render_board(board:, position:)
   edge = board.length - 1
   0.upto(edge) do |y|
     0.upto(edge) do |x|
       symbol = if position && x == position.x && y == position.y
                  "*#{board[x][y]}"
-               elsif new_position && x == new_position.x && y == new_position.y
-                 "Next"
                else
-                 "."
+                 board[x][y].to_s
                end
       print("[#{symbol.center(4)}]")
     end
@@ -50,9 +42,14 @@ def render_board(board:, position:, new_position:)
   end
 end
 
-def render_state(position:, new_position:, scored_moves:, move_number:)
-  puts("----------------------- Move #{move_number} -----------------------")
-  puts("position:#{position} new_position:#{new_position} scored_moves:#{scored_moves}")
+def render_header(move_number:, starting_position:, backtracking:)
+  puts("----------------------- Move #{move_number} (start was #{starting_position}) -----------------------")
+  puts("Currently backtracking") if backtracking
+end
+
+def render_state(position:, scored_moves:, position_history:)
+  puts("position:#{position} scored_moves:#{scored_moves}")
+  puts("position_history:#{position_history.join(' → ')}")
 end
 
 # The heuristic here is: choose the move with the lowest number of possible resulting moves
@@ -79,12 +76,14 @@ end
 def tour(board_size:, starting_position:, verbose:)
   board = Array.new(board_size) { Array.new(board_size) }
   position_history = [starting_position]
-  move_number = 1
+  move_number = 0
   scored_moves_history = []
   backtracking = false
 
-  while position_history.length < (board_size**2)
+  while position_history.length <= (board_size**2)
     move_number += 1
+
+    render_header(move_number:, backtracking:, starting_position:) if verbose
 
     if backtracking
       scored_moves = scored_moves_history.pop
@@ -95,51 +94,35 @@ def tour(board_size:, starting_position:, verbose:)
 
     new_position = scored_moves.shift&.first
 
-    if verbose
-      render_state(position: position_history.last, new_position:, scored_moves:, move_number: move_number)
-      render_board(board:, position: position_history.last, new_position:)
-    end
-
     if new_position
       backtracking = false
       position_history << new_position
       board[new_position.x][new_position.y] = position_history.length
     else
       backtracking = true
+      if scored_moves_history.empty?
+        raise "This combination (board_size #{board_size}; starting position #{starting_position}) has no solution."
+      end
     end
+    next unless verbose
 
+    render_state(position: position_history.last, scored_moves:,
+                 position_history:)
+    render_board(board:, position: position_history.last)
   end
 
   puts("Cool, we won! #{position_history.join(' → ')} (#{position_history.length} moves)") if verbose
 end
 
-# Will cause a backtrack
-tour(board_size: 11, starting_position: Point.new(1, 9), verbose: true)
+tour(board_size: 6, starting_position: Point.new(4, 4), verbose: true)
 
-Benchmark.ips do |bm|
-  bm.config(warmup: 2, time: 5)
+# Benchmark.ips do |bm|
+#   bm.config(warmup: 1, time: 5)
 
-  8.upto(15) do |board_size|
-    edge = board_size - 1
-    starting_positions = [
-      Point.new(x: 0, y: 0),
-      Point.new(x: 0, y: edge),
-      Point.new(x: edge, y: 0),
-      Point.new(x: edge, y: edge),
-      Point.new(x: 1, y: 1),
-      Point.new(x: 1, y: edge - 1),
-      Point.new(x: edge - 1, y: 1),
-      Point.new(x: edge - 1, y: edge - 1),
-      Point.new(x: edge / 2, y: edge / 2)
-    ]
-    bm.report("Board size: #{board_size} (#{starting_positions.length} starting positions)") do
-      # puts("Will benchmark board size #{board_size}")
-
-      starting_positions.each do |sp|
-        tour(board_size: board_size, starting_position: sp, verbose: false)
-      end
-    end
-  end
-
-  bm.compare!
-end
+#   4.upto(15) do |board_size|
+#     bm.report("Board size: #{board_size}") do
+#       start = Point.new(rand(0..board_size - 1), rand(0..board_size - 1))
+#       tour(board_size: board_size, starting_position: start, verbose: false)
+#     end
+#   end
+# end
