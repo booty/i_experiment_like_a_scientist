f1514ef7caa0e66b368729179f18b7e221495752
John Rose <johnedmundrose@gmail.com>
2025-11-20 13:28:28 -0500

    Neighbor caching optimization, nearly 2x speedup

--- ruby/knight/knight-smart.rb
+++ ruby/knight/knight-smart.rb
@@ -52,28 +52,28 @@ def render_state(position:, scored_moves:, position_history:)
   puts("position_history:#{position_history.join(' → ')}")
 end
 
-# The heuristic here is: choose the move with the lowest number of possible resulting moves
-def scored_moves(board:, position:, recurse:)
-  moves = DELTAS.filter_map do |delta|
-    new_position = position + delta
-
-    next unless new_position.inbounds?(board.length)
-    next if board[new_position.x][new_position.y] # knight has already visited
-
-    new_position
+def neighbors_for(size:, position:, neighbors_cache:)
+  neighbors_cache[position] ||= DELTAS.filter_map do |d|
+    p2 = position + d
+    p2 if p2.inbounds?(size)
   end
+end
 
-  return moves.map { |r| [r, 0] } unless recurse
+def moves_from(board:, position:, neighbors_cache:)
+  neighbors_for(size: board.length, position:, neighbors_cache:).reject { |p2| board[p2.x][p2.y] }
+end
 
-  moves.map do |move|
-    [
-      move,
-      scored_moves(board: board, position: move, recurse: false).length
-    ]
-  end.sort_by { |x| x[1] }
+# The heuristic here is: choose the move with the lowest number of possible
+# resulting moves (Warnsdorff’s...ish)
+def scored_moves(board:, position:, recurse:, neighbors_cache:)
+  moves = moves_from(board:, position:, neighbors_cache:)
+  return moves.map { [_1, 0] } unless recurse
+
+  moves.map { |m| [m, moves_from(board:, position: m, neighbors_cache:).size] }.sort_by(&:last)
 end
 
 def tour(board_size:, starting_position:, verbose:)
+  neighbors_cache = {}
   board = Array.new(board_size) { Array.new(board_size) }
   position_history = [starting_position]
   move_number = 0
@@ -88,7 +88,7 @@ def tour(board_size:, starting_position:, verbose:)
     if backtracking
       scored_moves = scored_moves_history.pop
     else
-      scored_moves = scored_moves(board:, position: position_history.last, recurse: true)
+      scored_moves = scored_moves(board:, position: position_history.last, recurse: true, neighbors_cache:)
       scored_moves_history.push(scored_moves)
     end
 
@@ -116,13 +116,13 @@ end
 
 tour(board_size: 6, starting_position: Point.new(4, 4), verbose: true)
 
-# Benchmark.ips do |bm|
-#   bm.config(warmup: 1, time: 5)
+Benchmark.ips do |bm|
+  bm.config(warmup: 0.5, time: 3)
 
-#   4.upto(15) do |board_size|
-#     bm.report("Board size: #{board_size}") do
-#       start = Point.new(rand(0..board_size - 1), rand(0..board_size - 1))
-#       tour(board_size: board_size, starting_position: start, verbose: false)
-#     end
-#   end
-# end
+  4.upto(15) do |board_size|
+    bm.report("Board size: #{board_size}") do
+      start = Point.new(rand(0..board_size - 1), rand(0..board_size - 1))
+      tour(board_size: board_size, starting_position: start, verbose: false)
+    end
+  end
+end
