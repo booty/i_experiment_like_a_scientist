4dd51c004b6ce92a03cbb73a0284a623c65f1b39
John Rose <johnedmundrose@gmail.com>
2025-11-30 18:03:30 -0500

    Functional but destructive

--- ruby/codeexams/deep_symbolize_keys.rb
+++ ruby/codeexams/deep_symbolize_keys.rb
@@ -15,34 +15,33 @@
 # deep_symbolize({ "a" => { "b" => [ { "c" => 1 } ] } })
 
 def deep_symbolize(obj)
-  # case obj.class
-  # when Array
-  #   raise "Not implemented"
-  # when Hash
-  # end
+  if obj.is_a?(Hash)
+    obj.keys.each do |k|
+      unless k.is_a?(Symbol)
+        # convert the key
+        v = obj[k]
+        new_k = k.to_sym
+        obj[new_k] = v
+        obj.delete(k)
+        k = new_k
+      end
 
-  # obj.each do |k, v|
-  obj.keys.each do |k|
-    # replace the key if it needs replacing
-    # raise "Unknown key type: #{k.class}" unless k.is_a?(String)
-
-    unless k.is_a?(Symbol)
-      # convert the key
       v = obj[k]
-      new_k = k.to_sym
-      obj[new_k] = v
-      obj.delete(k)
-      k = new_k
+
+      # puts "  #{v.class} #{v.class === Hash}"
+      case v
+      when Array, Hash
+        # puts "  will deep_symbolize #{v}"
+        obj[k] = deep_symbolize(v)
+      end
     end
+    return obj
+  end
 
-    puts ""
+  return obj unless obj.is_a?(Array)
 
-    # do the deep part
-    # case v.class
-
-    # when String, Hash
-    #   deep_symbolize(v)
-    # end
+  obj.each_index do |i|
+    obj[i] = deep_symbolize(obj[i])
   end
   obj
 end
@@ -50,7 +49,12 @@ end
 inputs = [
   { cat: 1, dog: 2 },
   { "a" => 1, "b" => 2 },
-  { :a => 1, "a" => 2, :b => 42, "c" => 3 }
+  { :a => 1, "a" => 2, :b => 42, "c" => 3 },
+  { "a" => { "b" => [{ "c" => 1 }] } },
+  [1, 2, 3],
+  [[1, 2, 3], 4],
+  [[1, 2, 3, { "a" => 123 }], 4],
+  { "a" => [1, 2, 3] }
 ]
 
 inputs.each do |x|
