fff22b2797dd4cb03ea3405d0e76cfa1e871deae
John Rose <johnedmundrose@gmail.com>
2025-12-05 11:29:43 -0500

    Support ranges, arrays, stable multi-sort

--- ruby/codeexams/in_memory_query_engine.rb
+++ ruby/codeexams/in_memory_query_engine.rb
@@ -16,7 +16,7 @@ srand(12_345)
 
 FIRST_NAMES = {
   N: %w[Sutton Campbell Alexis Sawyer Skyler Stevie Landry Oakley Max Justice Baylor Greer Charlie Morgan Azariah
-        Dylan Bellamy Bellamy Greer Blake Song"],
+        Dylan Bellamy Bellamy Greer Blake Song],
   M: %w[James Robert John Michael David William Richard Joseph Thomas Charles Santos Rashad Sylvester Elijah Luther
         Joseph Alonso Antwan Rob Clifford],
   F: %w[Mary Patricia Jennifer Linda Elizabeth Barbara Susan Jessica Sarah Karen Chantel Abigail Casie Eleanor
@@ -28,7 +28,7 @@ LAST_NAMES =
      Lebsack Runolfsson Herman].deep_freeze
 
 DATA =
-  Array.new(10_000) do
+  Array.new(20_000) do
     gender = %i[M F N].sample
     { first_name: FIRST_NAMES[gender].sample, last_name: LAST_NAMES.sample, age: rand(18..40) }
   end
@@ -40,11 +40,13 @@ class QueryEngine
   end
 
   def order(*attributes)
-    result = @data
-    attributes.each do |attrib|
-      result = result.sort_by { |x| x[attrib] }
+    indexed = @data.each_with_index # sort and sort_by are not stable so we need to do a little extra work
+
+    sorted = indexed.sort_by do |row, idx|
+      attributes.map { |attr| row.fetch(attr) } << idx
     end
-    QueryEngine.new(result)
+
+    QueryEngine.new(sorted.map(&:first))
   end
 
   def limit(count)
@@ -55,18 +57,24 @@ end
 def where(data, query_hash)
   filtered = data
 
-  query_hash.each do |k, v|
-    if filtered.any? && !filtered[0].keys.include?(k)
-      raise ArgumentError, "Attribute not found in data: \"#{k}\""
-
+  query_hash.each do |filter_key, filter_value|
+    if filtered.any? && !filtered[0].keys.include?(filter_key)
+      raise ArgumentError, "Attribute not found in data: \"#{filter_key}\""
     end
 
     filtered = filtered.select do |item|
-      item[k] == v
+      if filter_value.is_a?(Array)
+        filter_value.include?(item[filter_key])
+      elsif filter_value.is_a?(Range)
+
+        filter_value.cover?(item[filter_key])
+      else
+        item[filter_key] == filter_value
+      end
     end
   end
 
   QueryEngine.new(filtered)
 end
 
-puts where(DATA, age: 30, first_name: "David").order(:first_name, :last_name, :age).limit(15)
+puts where(DATA, age: 30..35, first_name: %w[David Song]).order(:last_name, :age, :first_name).limit(20)
