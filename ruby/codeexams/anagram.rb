50f61be6adda8fef0e2e0eaecc9ba49c1e53afad
John Rose <johnedmundrose@gmail.com>
2025-12-13 21:16:28 -0500

    Optimize anagrams

--- ruby/codeexams/anagram.rb
+++ ruby/codeexams/anagram.rb
@@ -24,37 +24,49 @@ require "prettyprint"
 # Try to improve the performance of your solution but keep in mind that making it faster affects readability.
 # Continue to focus on readability first.
 
-# Assumes that the list is sorted
 def get_wordlist(path)
-  File.readlines(path).map(&:chomp)
+  File.readlines(path).map(&:chomp).sort
 end
 
 def string_exists_in_wordlist(wordlist:, target:)
   wordlist.bsearch { |x| x >= target } == target
 end
 
-def find_anagrams(chars:, wordlist:)
-  chars.permutation.each_with_object([]) do |perm, result|
+def find_anagrams(chars:, wordlist:, anagram_cache:)
+  cached_result = anagram_cache[chars]
+  return cached_result unless cached_result.nil?
+
+  result = chars.permutation.each_with_object([]) do |perm, result|
     joined = perm.join
     result << joined if string_exists_in_wordlist(wordlist:, target: joined)
   end
+
+  anagram_cache[chars] = result
 end
 
 def find_two_word_anagrams(chars:, wordlist:)
+  anagram_cache = {}
+
   chars.permutation.each_with_object(Set.new) do |perm, results|
     0.upto(perm.length - 2) do |cut_after|
       slice1 = perm[0..cut_after]
       slice2 = perm[(cut_after + 1)..]
 
-      anagrams_for_slice1 = find_anagrams(chars: slice1, wordlist:)
+      anagrams_for_slice1 = find_anagrams(chars: slice1, wordlist:, anagram_cache:)
 
       next if anagrams_for_slice1.empty?
 
-      anagrams_for_slice2 = find_anagrams(chars: slice2, wordlist:)
+      anagrams_for_slice2 = find_anagrams(chars: slice2, wordlist:, anagram_cache:)
 
       next if anagrams_for_slice2.empty?
 
-      results.merge(anagrams_for_slice1.product(anagrams_for_slice2))
+      next if slice2.length > slice1.length
+
+      foo = anagrams_for_slice1.product(anagrams_for_slice2).map(&:sort)
+
+      # puts("foo:#{foo}")
+
+      results.merge(foo)
     end
   end
 end
@@ -62,6 +74,13 @@ end
 wordlist = get_wordlist("words_alpha_sorted.txt")
 puts "Got #{wordlist.length} entries in the wordlist"
 
-answer = find_two_word_anagrams(chars: "laptop".chars, wordlist:)
-puts("answer has #{answer.length} entries and #{answer.uniq.length} uniques")
-puts("#{answer.uniq.sort}")
+%w[laptop].each do |word|
+  start_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+
+  answer = find_two_word_anagrams(chars: word.chars, wordlist:)
+  puts "Found #{answer.length} results: #{answer.sort}"
+
+  end_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+  elapsed = end_time - start_time
+  puts "Elapsed time for \"#{word}\": #{elapsed.round(4)} seconds"
+end
