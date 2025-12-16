4f5f3ff52c50fd9aaa2e877b060bc377114e3b22
John Rose <johnedmundrose@gmail.com>
2025-12-16 00:12:52 -0500

    Calculate tallies ahead of time

--- ruby/codeexams/anagram.rb
+++ ruby/codeexams/anagram.rb
@@ -22,13 +22,28 @@
 # Try to improve the performance of your solution but keep in mind that making it faster affects readability.
 # Continue to focus on readability first.
 
-def get_wordlist_by_length(path)
-  File.readlines(path).map(&:chomp).sort_by(&:length).map(&:downcase)
+require "stackprof"
+
+WORDLIST_PATH = "words_alpha_sorted.txt"
+
+def perf_time
+  Process.clock_gettime(Process::CLOCK_MONOTONIC)
 end
 
-def letter_count_tally(word:, tally_cache:)
-  return tally_cache[word] if tally_cache[word]
+def get_wordlist_by_length(path:, max_length:)
+  words = []
 
+  File.foreach(path) do |line|
+    word = line.chomp
+    next if word.length > max_length
+
+    words << [word.downcase, letter_count_tally(word.downcase)]
+  end
+
+  words.sort_by! { |word, _tally| word.length }
+end
+
+def letter_count_tally(word)
   stringbuffer = "\x00" * 26
   stringbuffer.force_encoding(Encoding::BINARY)
 
@@ -37,7 +52,7 @@ def letter_count_tally(word:, tally_cache:)
     stringbuffer.setbyte(pos, stringbuffer.getbyte(pos) + 1)
   end
 
-  tally_cache[word] = stringbuffer
+  stringbuffer
 end
 
 def remaining_letter_count_tally(stringbuffer1, stringbuffer2)
@@ -56,52 +71,42 @@ def remaining_letter_count_tally(stringbuffer1, stringbuffer2)
   stringbuffer_result
 end
 
-def find_two_word_anagrams(word:, wordlist:)
+def find_two_word_anagrams(word:)
   word = word.downcase
 
   results = Set.new
   word_length = word.length
-  tally_cache = {}
-  word_tally = letter_count_tally(word:, tally_cache:)
+  word_tally = letter_count_tally(word).freeze
 
-  candidates_sorted_by_length =
-    wordlist
-    .select { |x| x.length < word_length }
-  # .sort_by(&:length)
+  candidates_sorted_by_length = get_wordlist_by_length(path: WORDLIST_PATH, max_length: word_length - 1)
 
-  candidates_grouped_by_length = candidates_sorted_by_length.group_by(&:length)
+  candidates_grouped_by_length = candidates_sorted_by_length.group_by { |x| x[0].length }
 
-  candidates_sorted_by_length.each_with_index do |answer1, outer_index|
+  candidates_sorted_by_length.each_with_index do |(answer1, answer1_tally), _outer_index|
     break if answer1.length > word_length / 2
-
-    answer1_tally = letter_count_tally(word: answer1, tally_cache:)
 
     remaining_letters_tally = remaining_letter_count_tally(word_tally, answer1_tally)
 
     next unless remaining_letters_tally
 
-    remaining_letters_count = remaining_letters_tally.sum
+    # remaining_letters_count = remaining_letters_tally.sum
+    remaining_letters_count = word.length - answer1.length
 
     next unless remaining_letters_count.positive?
 
-    (candidates_grouped_by_length[remaining_letters_count] || []).each do |answer2|
-      answer_2_tally = letter_count_tally(word: answer2, tally_cache:)
-      results.add([answer1, answer2].sort) if remaining_letters_tally == answer_2_tally
+    (candidates_grouped_by_length[remaining_letters_count] || []).each do |(answer2, answer2_tally)|
+      results.add([answer1, answer2].sort) if remaining_letters_tally == answer2_tally
     end
   end
   results.sort
 end
 
-wordlist = get_wordlist_by_length("words_alpha_sorted.txt")
-puts "Got #{wordlist.length} entries in the wordlist"
-
 %w[documenting].each do |word|
-  start_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+  start_time = perf_time
 
-  answer = find_two_word_anagrams(word:, wordlist:)
+  answer = find_two_word_anagrams(word:)
   puts "Found #{answer.length} results: #{answer}"
 
-  end_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-  elapsed = end_time - start_time
+  elapsed = perf_time - start_time
   puts "Elapsed time for \"#{word}\": #{elapsed.round(4)} seconds"
 end
